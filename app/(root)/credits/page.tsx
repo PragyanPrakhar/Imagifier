a5f398548836c0cbac124925331bc9c7a3b33079
Pragyan Prakhar <pragyanprakhar@gmail.com>
2025-01-11 13:01:26 +0530

    Set the basic routing structure

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Creditpage = () => {
+    return <div>CreditPage</div>;
+};
+
+export default Creditpage;
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>
+      Transformations Page
+    </div>
+  )
+}
+
+export default TransformationsPage;
+
