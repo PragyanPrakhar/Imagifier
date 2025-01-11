349344538c3006bf4b09b9283ee4d07c2f0f9468
Pragyan Prakhar <pragyanprakhar@gmail.com>
2025-01-11 15:21:32 +0530

    Completed the sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,43 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
+import {
+    ClerkProvider,
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
 const IBMPlex = IBM_Plex_Sans({
-  subsets: ["latin"],
-  weight:['400','500','600','700'],
-  variable: '--font-ibm-plex'
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    variable: "--font-ibm-plex",
 });
 export const metadata: Metadata = {
-  title: "Imagifier",
-  description: "Ai enabled image processing",
+    title: "Imagifier",
+    description: "Ai enabled image processing",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={cn("font-IBMPlex antialiased",IBMPlex.variable)}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <ClerkProvider
+            appearance={{
+                variables: { colorPrimary: "#624cf5" },
+            }}
+        >
+            <html lang="en">
+                <body
+                    className={cn("font-IBMPlex antialiased", IBMPlex.variable)}
+                >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
